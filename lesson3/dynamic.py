af7460fe1a1fb353026bfbb5e0f6549f661db2a0
Stainley Lebron <saiyamans@gmail.com>
2020-02-09 11:50:16 -0400

    More practice

---
+++ lesson3/dynamic.py
@@ -0,0 +1,15 @@
+stored_result = {}
+
+
+def sum_to_n(n):
+    result = 0
+    for i in reversed(range(n)):
+        if i + 1 in stored_result:
+            print('Stopping sum at %s because we have previously computed it' % str(i + 1))
+            result += stored_result[i + 1]
+            break
+        else:
+            result += i + 1
+
+    stored_result[n] = result
+    return result
